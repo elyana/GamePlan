172fac0f13c619fd9c3da30d4e1a0a2e0a926dbb
courtbohrer <cbohrer@utexas.edu>
2014-02-22 14:47:39 -0600

    Initial Commit

---
+++ GamePlan/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GamePlan
+//
+//  Created by Courtney Bohrer on 2/22/14.
+//  Copyright (c) 2014 Courtney Bohrer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SimpleTableAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SimpleTableAppDelegate class]));
+    }
+}
---
+++ GamePlanTests/GamePlanTests.m
@@ -0,0 +1,34 @@
+//
+//  GamePlanTests.m
+//  GamePlanTests
+//
+//  Created by Courtney Bohrer on 2/22/14.
+//  Copyright (c) 2014 Courtney Bohrer. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GamePlanTests : XCTestCase
+
+@end
+
+@implementation GamePlanTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
